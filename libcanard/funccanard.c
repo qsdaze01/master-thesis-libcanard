f027f235fc608625b26b668f57fb7fcf207a1afd
qsdaze01 <gaspard.michel@insa-lyon.fr>
2023-09-17 20:33:05 +0200

    ADD : stuffed bits functions

--- libcanard/funccanard.c
+++ libcanard/funccanard.c
@@ -24,22 +24,70 @@ uint16_t computeCRC(int payload_size, uint8_t * buffer_frame) {
 
     uint8_t start_frame_size = 19 + 8*payload_size;
     int temp = 0;
-    printf("CRC buffer : ");
     for (int i = 0; i < start_frame_size; i++){
         if (i%8 == 0 && i != 0){
             temp++;
         }
         byte_buffer[temp] += buffer_frame[i]*pow(2,7-i%8);
-        printf("%d ", buffer_frame[i]);
     }
-    printf("\n");
-    printf("Byte buffer CRC computation : ");
-    for (int i = 0; i < payload_size + 3; i++) {
-        printf("%d ", byte_buffer[i]);
-    }
-    printf("\n");
     uint16_t CRC = crcAdd(0, 3 + payload_size, byte_buffer);
     return CRC;
+}
+
+uint8_t instertStuffedBits (uint8_t* buffer_in, uint8_t* buffer_out, uint8_t payload_size) {
+    uint8_t nb_stuffed_bits = 0;
+    uint8_t count_successive_1 = 0;
+    uint8_t count_successive_0 = 0;
+    for (int i = 0; i < 44 + 8*payload_size; i++) {
+        if (buffer_in[i] == 0) {
+            count_successive_1 = 0;
+            count_successive_0++;
+        } 
+        if (buffer_in[i] == 1) {
+            count_successive_0 = 0;
+            count_successive_1++;
+        }
+        buffer_out[i + nb_stuffed_bits] = buffer_in[i];
+        if (count_successive_0 == 5 || count_successive_1 == 5) {
+            nb_stuffed_bits++;
+            count_successive_0 = 0;
+            count_successive_1 = 0;
+            if (buffer_in[i] == 0) {
+                buffer_out[i + nb_stuffed_bits] = 1;
+            } else {
+                buffer_out[i + nb_stuffed_bits] = 0;
+            }
+        }
+    }
+    return nb_stuffed_bits;
+}
+
+uint8_t removeStuffedBits(uint8_t* buffer_in, uint8_t* buffer_out, uint8_t buffer_size){
+    uint8_t nb_stuffed_bits = 0;
+    uint8_t count_successive_1 = 0;
+    uint8_t count_successive_0 = 0;
+    for (int i = 0; i < buffer_size; i++) {
+        buffer_out[i] = buffer_in[i + nb_stuffed_bits];
+        if (buffer_in[i + nb_stuffed_bits] == 0) {
+            count_successive_1 = 0;
+            count_successive_0++;
+        } 
+        if (buffer_in[i + nb_stuffed_bits] == 1) {
+            count_successive_0 = 0;
+            count_successive_1++;
+        }
+        if (count_successive_0 == 5) {
+            count_successive_0 = 0;
+            count_successive_1 = 0;
+            nb_stuffed_bits++;
+        } 
+        if (count_successive_1 == 5) {
+            count_successive_0 = 0;
+            count_successive_1 = 0;
+            nb_stuffed_bits++;
+        }
+    }
+    return nb_stuffed_bits;
 }
 
 /*
@@ -90,7 +138,6 @@ void serializeFrame(int payload_size, uint8_t* payload, uint16_t identifier, uin
     }
     //CRC
     CRC = computeCRC(payload_size, buffer);
-    printf("Sent CRC : %d \n", CRC);
     for (int i = 14; i >= 0; i--) {
         uint16_t mask = 1 << i;
         uint8_t bit = (CRC & mask) ? 1 : 0;
@@ -110,11 +157,6 @@ void serializeFrame(int payload_size, uint8_t* payload, uint16_t identifier, uin
     for (int i = 0; i < 3; i++){
         buffer[44 + 8*payload_size + i] = 1;
     }
-    printf("Sent frame : ");
-    for (int i = 0; i < 47 + 8*payload_size; i++) {
-        printf("%d ", buffer[i]);
-    }
-    printf("\n");    
 }
 
 uint8_t deserializeFrame(uint8_t *buffer, uint16_t* identifier, uint8_t buffer_size){
@@ -124,7 +166,6 @@ uint8_t deserializeFrame(uint8_t *buffer, uint16_t* identifier, uint8_t buffer_s
     for (int i = 0; i < 11; i++) {
         *identifier += buffer[1 + i]*pow(2,10-i);
     }
-    printf("Identifier : %d \n", *identifier);
 
     uint64_t data = 0;
     for (int i = 0; i < 8*payload_size; i++){
@@ -135,9 +176,6 @@ uint8_t deserializeFrame(uint8_t *buffer, uint16_t* identifier, uint8_t buffer_s
     for (int i = 0; i < 15; i++){
         CRC_received += buffer[19 + 8*payload_size + i]*pow(2,14-i);
     }
-
-    printf("Computed CRC : %d \n", CRC_computed);
-    printf("Received CRC : %d \n", CRC_received);
 
     if (!(CRC_computed == CRC_received || CRC_computed - pow(2,15) == CRC_received)) {
         printf("error \n");
@@ -183,8 +221,6 @@ int transmitFrame(CanardTxQueue queue, CanardInstance canard, uint8_t tx_deadlin
         printf("Error \n");
         return 1;
     }
-
-    printf("Queue size : %ld \n", queue.size);
 
     for (const CanardTxQueueItem* ti = NULL; (ti = canardTxPeek(&queue)) != NULL;)  // Peek at the top of the queue.
     {
--- libcanard/funccanard.h
+++ libcanard/funccanard.h
@@ -10,6 +10,10 @@ void serializeFrame(int payload_size, uint8_t* payload, uint16_t identifier, uin
 
 uint8_t deserializeFrame(uint8_t *buffer, uint16_t* identifier, uint8_t buffer_size);
 
+uint8_t instertStuffedBits (uint8_t* buffer_in, uint8_t* buffer_out, uint8_t payload_size);
+
+uint8_t removeStuffedBits(uint8_t* buffer_in, uint8_t* buffer_out, uint8_t buffer_size);
+
 int pleaseTransmit(CanardTxQueueItem* ti, CanardTransferMetadata transfer_metadata);
 
 int transmitFrame(CanardTxQueue queue, CanardInstance canard, uint8_t tx_deadline_usec, uint8_t *my_message_transfer_id, int port_id, int payload_size, char* payload);
